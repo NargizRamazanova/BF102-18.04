fcf4e1e951590fa4fcc842799b4d5eea4d174a05
NargizRamazanova <116425809+NargizRamazanova@users.noreply.github.com>
2023-04-18 10:27:45 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -68,7 +68,164 @@
 // });
 
 
-// 
+// let data = [
+//     {
+//         img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+//         name: "Fancy Product",
+//         price: "$40.00 - $80.00",
+//         details: "https://startbootstrap.com/previews/shop-homepage",
+//         isOnSale: false,
+//     },
+//     {
+//         img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+//         name: "Special Item",
+//         price: "$40.00 - $80.00",
+//         details: "https://startbootstrap.com/previews/shop-homepage",
+//         isOnSale: true,
+
+//     },
+//     {
+//         img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+//         name: "Sale Item",
+//         price: "$40.00 - $80.00",
+//         details: "https://startbootstrap.com/previews/shop-homepage",
+//         isOnSale: false,
+
+//     },
+//     {
+//         img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+//         name: "Popular Item",
+//         price: "$40.00 - $80.00",
+//         details: "https://startbootstrap.com/previews/shop-homepage",
+//         isOnSale: false,
+
+//     },
+//     {
+//         img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+//         name: "Sale Item",
+//         price: "$40.00 - $80.00",
+//         details: "https://startbootstrap.com/previews/shop-homepage",
+//         isOnSale: true,
+
+//     },
+//     {
+//         img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+//         name: "Fancy Product",
+//         price: "$40.00 - $80.00",
+//         details: "https://startbootstrap.com/previews/shop-homepage",
+//         isOnSale: false,
+
+//     },
+//     {
+//         img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+//         name: "Popular Item",
+//         price: "$40.00 - $80.00",
+//         details: "https://startbootstrap.com/previews/shop-homepage",
+//         isOnSale: false,
+//     },
+//     {
+//         img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+//         name: "Special Item",
+//         price: "$40.00 - $80.00",
+//         details: "https://startbootstrap.com/previews/shop-homepage",
+//         isOnSale: true,
+
+//     },
+
+// ]
+
+
+// const container = document.createElement("div")
+// container.classList.add("container")
+// container.classList.add("pt-5")
+// document.querySelector("body").appendChild(container)
+
+// const row = document.createElement("div")
+// row.classList.add("row")
+// container.appendChild(row)
+
+
+// data.forEach(element => {
+//     let col= document.createElement("div")
+//     col.classList.add("col-3")
+//     col.classList.add("mb-3")
+
+//     let card = document.createElement("div")
+//     card.classList.add("card")
+//     card.style.position = "relative"
+
+//     if(element.isOnSale){
+//         const badge =document.createElement("span")
+//         badge.classList.add("badge")
+//         badge.classList.add("bg-dark")
+//         badge.innerText = "Sale"
+//         badge.style.position = "absolute"
+//         badge.style.top = "10px"
+//         badge.style.right = "10px"
+//         card.append(badge)
+
+//     }
+
+//     let image = document.createElement("img")
+//     image.classList.add("card-img-top")
+//     image.setAttribute("src", element.img)
+
+//     let cardBody = document.createElement("div")
+//     cardBody.classList.add("card-body")
+
+//     let heading = document.createElement("h5")
+//     heading.classList.add("card-title")
+//     heading.innerText = element.name
+
+//     let price = document.createElement("p")
+//     price.classList.add("card-text")
+//     price.innerText = element.price
+
+//     let btn = document.createElement("a")
+//     btn.classList.add("btn-primary")
+//     btn.setAttribute("href", element.details)
+
+//     cardBody.append(heading, price, btn)
+//     card.append(image, cardBody)
+//     col.append(card)
+//     row.append(col)
+// })
+
+
+
+
+// Uch input yaradirsiz ve bir button. Inputlarla fullName, email ve age goturursuz.
+// Button-a click olunanda bu melumatlarla bir user yaradirsiz.
+// User bir obyektir ve butun userler array de saxlanilir.
+// User-lerin siyahi ashagidan listin ichinde gorunur
+
+const users = []
+
+const nameInput = document.querySelector("#name")
+const emailInput = document.querySelector("#email")
+const ageInput = document.querySelector("#age")
+const btn = document.querySelector("button")
+const ul = document.querySelector("ul")
+
+
+btn.addEventListener("click", function(){
+    const li = document.createElement("li");
+    let newUser = {
+        name: nameInput.value,
+        email: emailInput.value,
+        age: ageInput.value
+    }
+    users.push(newUser)
+    li.innerText = `Name: ${nameInput.value}, Email:${emailInput.value}, Age: ${ageInput.value}`
+    ul.append(li)
+    nameInput.value="";
+    emailInput.value="";
+    ageInput.value=0;
+    console.log(users)
+})
+
+
+
 
 
 
