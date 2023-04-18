cfd908145e03dbf011c422db1e7a9d763d8bd702
NargizRamazanova <116425809+NargizRamazanova@users.noreply.github.com>
2023-04-18 08:42:41 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+// let arr = [
+//     {
+//         img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+//         title: "Lorem ipsum ",
+//         desc: "Lorem ipsum dolor sit amet.",
+//         link: "facebook.com"
+//     },
+//     {
+//         img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+//         title: "Lorem ",
+//         desc: "Lorem ipsum dolor sit amet.",
+//         link: "facebook.com"
+//     },
+//     {
+//         img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+//         title: "Lorem ipsum dolor ",
+//         desc: "Lorem ipsum dolor sit amet.",
+//         link: "facebook.com"
+//     },
+//     {
+//         img: "https://dummyimage.com/450x300/dee2e6/6c757d.jpg",
+//         title: "Lorem ipsum dolor ",
+//         desc: "Lorem ipsum dolor sit amet.",
+//         link: "facebook.com"
+//     },
+
+// ]
+
+// const body = document.querySelector("body")
+// body.classList.add("pt-5");
+// let container = document.createElement("div");
+// container.classList.add("container");
+// body.append(container);
+
+
+// let Div = document.createElement("div");
+// Div.classList.add("mb-4");
+// Div.style.backgroundColor = "black";
+// Div.style.height = "360px";
+// container.appendChild(Div);
+
+// let row = document.createElement("div");
+// row.classList.add("row");
+// container.append(row)
+
+
+// arr.forEach(element => {
+//     let col4 = document.createElement("div")
+//     col4.classList.add("col-3");
+
+//     let img = document.createElement("img");
+//     img.setAttribute("src" , element.img)
+//     img.style.width = "100%"
+
+//     let title = document.createElement("h4");
+//     title.innerText = element.title;
+
+//     let desc = document.createElement("p");
+//     desc.innerText = element.desc;
+
+//     let link = document.createElement("a");
+//     link.setAttribute("href" , element.link);
+//     link.innerText = "read more";
+
+//     col4.append(img,title,desc,link);
+//     row.append(col4);
+
+// });
+
+
+// 
+
+
+
+
+
+
+
